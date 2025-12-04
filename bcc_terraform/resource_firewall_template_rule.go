6b68c373f78359e4bf5172c2a469e83de6021282
sa1tbaby <erpenteleychuk@basistech.ru>
2025-12-04 15:09:18 +0300

    update: FirewallTemplate add import additional logging and remove setId from read context

--- bcc_terraform/resource_firewall_template_rule.go
+++ bcc_terraform/resource_firewall_template_rule.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/basis-cloud/bcc-go/bcc"
@@ -24,7 +25,7 @@ func resourceFirewallRule() *schema.Resource {
 		UpdateContext: resourceFirewallRuleUpdate,
 		DeleteContext: resourceFirewallRuleDelete,
 		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+			StateContext: resourceFirewallImport,
 		},
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(10 * time.Minute),
@@ -36,61 +37,77 @@ func resourceFirewallRule() *schema.Resource {
 
 func resourceFirewallRuleCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	manager := meta.(*CombinedConfig).Manager()
-	firewall_id := d.Get("firewall_id").(string)
-	firewall, err := manager.GetFirewallTemplate(firewall_id)
+
+	firewallId := d.Get("firewall_id").(string)
+	firewall, err := manager.GetFirewallTemplate(firewallId)
 	if err != nil {
-		return diag.Errorf("firewall_id: Error getting FirewallTemplate: %s", err)
+		return diag.Errorf("[ERROR-048]: crash via getting FirewallTemplate by id=%s: %s", firewallId, err)
 	}
-	protocol := d.Get("protocol").(string)
+
 	var newFirewallRule bcc.FirewallRule
 	newFirewallRule.Name = d.Get("name").(string)
 	newFirewallRule.Direction = d.Get("direction").(string)
 	newFirewallRule.Protocol = d.Get("protocol").(string)
 	newFirewallRule.DestinationIp = d.Get("destination_ip").(string)
-	if protocol == "tcp" || protocol == "udp" {
+
+	if newFirewallRule.Protocol == "tcp" || newFirewallRule.Protocol == "udp" {
 		err = setUpRule(&newFirewallRule, d)
 		if err != nil {
-			return diag.Errorf("port_range: Error creating FirewallRule: %s", err)
+			return diag.Errorf("[ERROR-048]: crash vid setup FirewallRule: %s", err)
 		}
 	}
+
 	if err = firewall.CreateFirewallRule(&newFirewallRule); err != nil {
-		return diag.Errorf("Error creating FirewallRule: %s", err)
+		return diag.Errorf("[ERROR-048]: crash via creating FirewallRule: %s", err)
 	}
+
 	d.SetId(newFirewallRule.ID)
-	log.Printf("[INFO] Firewall Rule created, ID: %s", d.Id())
+	log.Printf("[INFO-048]: firewall Rule created, ID: %s", d.Id())
+
 	return resourceFirewallRuleRead(ctx, d, meta)
 }
 
 func resourceFirewallRuleRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	manager := meta.(*CombinedConfig).Manager()
-	firewall_id := d.Get("firewall_id").(string)
-	firewallRule_id := d.Id()
 
-	firewall, err := manager.GetFirewallTemplate(firewall_id)
+	firewallId := d.Get("firewall_id").(string)
+	firewall, err := manager.GetFirewallTemplate(firewallId)
 	if err != nil {
-		return diag.Errorf("firewall_id: Error getting Firewall Template: %s", err)
+		return diag.Errorf("[ERROR-048}: crash via getting Firewall Template by id=%s: %s", firewallId, err)
 	}
 
-	firewallRule, err := firewall.GetRuleById(firewallRule_id)
+	firewallRuleId := d.Id()
+	firewallRule, err := firewall.GetRuleById(firewallRuleId)
 	if err != nil {
 		if err.(*bcc.ApiError).Code() == 404 {
 			d.SetId("")
 			return nil
 		} else {
-			return diag.Errorf("id: Error getting fierwall Rule: %s", err)
+			return diag.Errorf("[ERROR-048]: crash via getting fierwall Rule by id=%s: %s", firewallRuleId, err)
 		}
 	}
 
-	d.SetId(firewallRule.ID)
-	d.Set("direction", firewallRule.Direction)
-	d.Set("name", firewallRule.Name)
-	d.Set("destination_ip", firewallRule.DestinationIp)
-	d.Set("protocol", firewallRule.Protocol)
+	fields := map[string]interface{}{
+		"firewall_id":    firewall.ID,
+		"name":           firewallRule.Name,
+		"direction":      firewallRule.Direction,
+		"protocol":       firewallRule.Protocol,
+		"destination_ip": firewallRule.DestinationIp,
+	}
+
+	if err = setResourceDataFromMap(d, fields); err != nil {
+		return diag.FromErr(err)
+	}
+
 	if firewallRule.DstPortRangeMin != nil {
-		d.Set("port_range", fmt.Sprintf("%d", *firewallRule.DstPortRangeMin))
+		if err = d.Set("port_range", fmt.Sprintf("%d", *firewallRule.DstPortRangeMin)); err != nil {
+			return diag.Errorf("[ERROR-048]: error setting MIN port_range: %s", err)
+		}
 	}
 	if firewallRule.DstPortRangeMax != nil {
-		d.Set("port_range", fmt.Sprintf("%s:%d", d.Get("port_range").(string), *firewallRule.DstPortRangeMax))
+		if err = d.Set("port_range", fmt.Sprintf("%s:%d", d.Get("port_range").(string), *firewallRule.DstPortRangeMax)); err != nil {
+			return diag.Errorf("[ERROR-049]: error setting MAX port_range: %s", err)
+		}
 	}
 
 	return nil
@@ -98,31 +115,30 @@ func resourceFirewallRuleRead(ctx context.Context, d *schema.ResourceData, meta 
 
 func resourceFirewallRuleUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	manager := meta.(*CombinedConfig).Manager()
-	firewall_id := d.Get("firewall_id").(string)
-	firewallRule_id := d.Id()
 
-	firewall, err := manager.GetFirewallTemplate(firewall_id)
+	firewallId := d.Get("firewallId").(string)
+	firewall, err := manager.GetFirewallTemplate(firewallId)
 	if err != nil {
-		return diag.Errorf("firewall_id: Error getting Firewall Template: %s", err)
+		return diag.Errorf("[ERROR-048}: crash via getting Firewall Template by id=%s: %s", firewallId, err)
 	}
 
-	firewallRule, err := firewall.GetRuleById(firewallRule_id)
+	firewallRuleId := d.Id()
+	firewallRule, err := firewall.GetRuleById(firewallRuleId)
 	if err != nil {
-		return diag.Errorf("id: Error getting fierwall Rule: %s", err)
+		return diag.Errorf("[ERROR-048]: crash via getting fierwall Rule by id=%S: %s", firewallRuleId, err)
 	}
 
 	firewallRule.Name = d.Get("name").(string)
-	protocol := d.Get("protocol").(string)
 	firewallRule.DestinationIp = d.Get("destination_ip").(string)
 	firewallRule.Protocol = d.Get("protocol").(string)
-	if protocol == "tcp" || protocol == "udp" {
+	if firewallRule.Protocol == "tcp" || firewallRule.Protocol == "udp" {
 		err = setUpRule(firewallRule, d)
 		if err != nil {
-			return diag.Errorf("port_range: Error updating FirewallRule: %s", err)
+			return diag.Errorf("[ERROR-048]: crash via setting up FirewallRule: %s", err)
 		}
 	}
 	if err = firewallRule.Update(); err != nil {
-		return diag.Errorf("Error updating Fierwall rule: %s", err)
+		return diag.Errorf("[ERROR-048]: crash via updating Fierwall rule: %s", err)
 	}
 
 	return resourceFirewallRuleRead(ctx, d, meta)
@@ -130,26 +146,26 @@ func resourceFirewallRuleUpdate(ctx context.Context, d *schema.ResourceData, met
 
 func resourceFirewallRuleDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	manager := meta.(*CombinedConfig).Manager()
-	firewall_id := d.Get("firewall_id").(string)
-	firewallRule_id := d.Id()
 
-	firewall, err := manager.GetFirewallTemplate(firewall_id)
+	firewallId := d.Get("firewallId").(string)
+	firewall, err := manager.GetFirewallTemplate(firewallId)
 	if err != nil {
-		return diag.Errorf("firewall_id: Error getting Firewall Template: %s", err)
+		return diag.Errorf("[ERROR-048}: crash via getting Firewall Template by id=%s: %s", firewallId, err)
 	}
 
-	firewallRule, err := firewall.GetRuleById(firewallRule_id)
+	firewallRuleId := d.Id()
+	firewallRule, err := firewall.GetRuleById(firewallRuleId)
 	if err != nil {
-		return diag.Errorf("id: Error getting fierwall Rule: %s", err)
+		return diag.Errorf("[ERROR-048]: crash via getting fierwall Rule by id=%S: %s", firewallRuleId, err)
 	}
 
 	err = firewallRule.Delete()
 	if err != nil {
-		return diag.Errorf("Error deleting Fierwall rule: %s", err)
+		return diag.Errorf("[ERROR-048]: crash via deleting Fierwall rule id=%s: %s", firewallId, err)
 	}
 
 	d.SetId("")
-	log.Printf("[INFO] Fierwall rule deleted, ID: %s", firewallRule_id)
+	log.Printf("[INFO-048] Fierwall rule deleted, ID: %s", firewallRuleId)
 	return nil
 }
 
@@ -177,4 +193,33 @@ func setUpRule(rule *bcc.FirewallRule, d *schema.ResourceData) (err error) {
 	}
 
 	return nil
+}
+
+func resourceFirewallImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	manager := meta.(*CombinedConfig).Manager()
+
+	id := d.Id()
+	ids := strings.Split(id, ",")
+
+	firewall, err := manager.GetFirewallTemplate(ids[0])
+	if err != nil {
+		return nil, fmt.Errorf("[ERROR-048}: crash via getting Firewall Template by id=%s: %s", ids[0], err)
+	}
+
+	firewallRule, err := firewall.GetRuleById(ids[1])
+	if err != nil {
+		if err.(*bcc.ApiError).Code() == 404 {
+			d.SetId("")
+			return nil, nil
+		} else {
+			return nil, fmt.Errorf("[ERROR-048]: crash via getting fierwall Rule by id=%s: %s", ids[1], err)
+		}
+	}
+
+	d.SetId(firewallRule.ID)
+	if err = d.Set("firewall_id", firewall.ID); err != nil {
+		return nil, fmt.Errorf("[ERROR-048]: error setting firewall_id: %s", err)
+	}
+
+	return []*schema.ResourceData{d}, nil
 }

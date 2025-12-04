bf6bb06c0073d9030b2d68cc1d24222b323a6b83
sa1tbaby <erpenteleychuk@basistech.ru>
2025-12-04 15:42:42 +0300

    update: add import for lbaas

--- bcc_terraform/resource_lbaas.go
+++ bcc_terraform/resource_lbaas.go
@@ -21,7 +21,7 @@ func resourceLbaas() *schema.Resource {
 		UpdateContext: resourceLbaasUpdate,
 		DeleteContext: resourceLbaasDelete,
 		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+			StateContext: resourceLbaasImport,
 		},
 		Schema: args,
 	}
@@ -180,4 +180,20 @@ func resourceLbaasDelete(ctx context.Context, d *schema.ResourceData, meta inter
 	log.Printf("[INFO-049] Lbaas deleted, ID: %s", lbaasId)
 
 	return nil
+}
+
+func resourceLbaasImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	manager := meta.(*CombinedConfig).Manager()
+	lbaas, err := manager.GetLoadBalancer(d.Id())
+	if err != nil {
+		if err.(*bcc.ApiError).Code() == 404 {
+			d.SetId("")
+			return nil, fmt.Errorf("[ERROR-049]: Lbaas not found")
+		} else {
+			return nil, fmt.Errorf("[ERROR-049]: crash via getting Lbaas by 'id'=%s: %s", d.Id(), err)
+		}
+	}
+
+	d.SetId(lbaas.ID)
+	return []*schema.ResourceData{d}, nil
 }

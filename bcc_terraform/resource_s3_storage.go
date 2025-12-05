d4831de072c97e9d83472aa8565e600645d2206e
sa1tbaby <erpenteleychuk@basistech.ru>
2025-12-05 14:17:22 +0300

    update: add additional loging for S3 storage

--- bcc_terraform/resource_s3_storage.go
+++ bcc_terraform/resource_s3_storage.go
@@ -2,6 +2,7 @@ package bcc_terraform
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -35,21 +36,25 @@ func resourceS3StorageCreate(ctx context.Context, d *schema.ResourceData, meta i
 	manager := meta.(*CombinedConfig).Manager()
 	project, err := GetProjectById(d, manager)
 	if err != nil {
-		return diag.Errorf("project_id: Error getting Project: %s", err)
+		return diag.Errorf("[ERROR-051]: crash via getting Project: %s", err)
 	}
+
 	name := d.Get("name").(string)
 	backend := d.Get("backend").(string)
+
 	newS3Storage := bcc.NewS3Storage(name, backend)
 	newS3Storage.Tags = unmarshalTagNames(d.Get("tags"))
 
 	err = project.CreateS3Storage(&newS3Storage)
 	if err != nil {
-		return diag.Errorf("Error creating S3Storage: %s", err)
+		return diag.Errorf("[ERROR-051]: crash via creating S3Storage: %s", err)
 	}
 
-	newS3Storage.WaitLock()
+	if err = newS3Storage.WaitLock(); err != nil {
+		return diag.Errorf("[ERROR-051]: crash via waiting for S3Storage to be created: %s", err)
+	}
 	d.SetId(newS3Storage.ID)
-	log.Printf("[INFO] S3Storage created, ID: %s", d.Id())
+	log.Printf("[INFO-051] S3Storage created, ID: %s", d.Id())
 
 	return resourceS3StorageRead(ctx, d, meta)
 }
@@ -59,7 +64,7 @@ func resourceS3StorageUpdate(ctx context.Context, d *schema.ResourceData, meta i
 
 	s3, err := manager.GetS3Storage(d.Id())
 	if err != nil {
-		return diag.Errorf("Error getting S3Storage: %s", err)
+		return diag.Errorf("[ERROR-051]: crash via getting S3Storage by 'id'=%s: %s", d.Id(), err)
 	}
 	if d.HasChange("name") {
 		s3.Name = d.Get("name").(string)
@@ -68,11 +73,13 @@ func resourceS3StorageUpdate(ctx context.Context, d *schema.ResourceData, meta i
 		s3.Tags = unmarshalTagNames(d.Get("tags"))
 	}
 
-	err = s3.Update()
-	if err != nil {
-		return diag.Errorf("Error updating S3Storage: %s", err)
+	if err = s3.Update(); err != nil {
+		return diag.Errorf("[ERROR-051]: crash via updating S3Storage: %s", err)
 	}
-	s3.WaitLock()
+	if err = s3.WaitLock(); err != nil {
+		return diag.Errorf("[ERROR-051]: crash via waiting for S3Storage to be created: %s", err)
+
+	}
 	log.Printf("[INFO] S3Storage updated, ID: %s", d.Id())
 
 	return resourceS3StorageRead(ctx, d, meta)
@@ -80,13 +87,13 @@ func resourceS3StorageUpdate(ctx context.Context, d *schema.ResourceData, meta i
 
 func resourceS3StorageRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	manager := meta.(*CombinedConfig).Manager()
-	S3Storage, err := manager.GetS3Storage(d.Id())
+	s3Storage, err := manager.GetS3Storage(d.Id())
 	if err != nil {
 		if err.(*bcc.ApiError).Code() == 404 {
 			d.SetId("")
 			return nil
 		} else {
-			return diag.Errorf("id: Error getting S3Storage: %s", err)
+			return diag.Errorf("[ERROR-051]: crash via getting S3Storage by 'id'=%s3Storage: %s3Storage", d.Id(), err)
 		}
 	}
 
@@ -109,20 +116,18 @@ func resourceS3StorageRead(ctx context.Context, d *schema.ResourceData, meta int
 
 func resourceS3StorageDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	manager := meta.(*CombinedConfig).Manager()
-	s3_id := d.Id()
 	s3, err := manager.GetS3Storage(d.Id())
 	if err != nil {
-		return diag.Errorf("id: Error getting S3Storage: %s", err)
+		return diag.Errorf("[ERROR-051]: crash via getting S3Storage by 'id'=%s: %s", d.Id(), err)
 	}
 
-	err = s3.Delete()
-	if err != nil {
-		return diag.Errorf("Error deleting S3Storage: %s", err)
+	if err = s3.Delete(); err != nil {
+		return diag.Errorf("[ERROR-051]: crash via deleting S3Storage: %s", err)
 	}
 	s3.WaitLock()
 
 	d.SetId("")
-	log.Printf("[INFO] S3Storage deleted, ID: %s", s3_id)
+	log.Printf("[INFO-051] S3Storage deleted, ID: %s", d.Id())
 
 	return nil
 }

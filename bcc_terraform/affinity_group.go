1ccfa767ad274a5f13f07e2e505c670b2c7f8557
sa1tbaby <erpenteleychuk@basistech.ru>
2025-12-09 15:26:29 +0300

    update: add affinity group

---
+++ bcc_terraform/affinity_group.go
@@ -0,0 +1,71 @@
+package bcc_terraform
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func (args *Arguments) injectCreateAffinityGroup() {
+	args.merge(Arguments{
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+			ValidateFunc: validation.All(
+				validation.NoZeroValues,
+				validation.StringLenBetween(1, 100),
+			),
+			Description: "name of the affinity group",
+		},
+		"description": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Default:     "",
+			Description: "description of the affinity group",
+		},
+		"policy": {
+			Type:     schema.TypeString,
+			Required: true,
+			ForceNew: true,
+			ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+				v := val.(string)
+				validValues := map[string]bool{
+					"soft-affinity":   true,
+					"strong-affinity": true,
+				}
+				if !validValues[v] {
+					errs = append(errs, fmt.Errorf("%q must be one of 'soft-affinity' or 'strong-affinity'", key))
+				}
+				return
+			},
+			Description: "The affinity type. Can be either 'soft-affinity' or 'strong-affinity'.",
+		},
+		"reboot": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Default:     false,
+			Description: "Whether to reboot the affinity group",
+		},
+		"vms": {
+			Type:        schema.TypeList,
+			Optional:    true,
+			Computed:    true,
+			Description: "List of VMs",
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"id": {
+						Type:        schema.TypeString,
+						Required:    true,
+						Description: "Id of the vm",
+					},
+					"name": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "Name of the vm",
+					},
+				},
+			},
+		},
+	})
+}
---
+++ bcc_terraform/resource_affinity_group.go
@@ -0,0 +1,172 @@
+package bcc_terraform
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/basis-cloud/bcc-go/bcc"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceAffinityGroup() *schema.Resource {
+	args := Defaults()
+	args.injectCreateAffinityGroup()
+	args.injectContextVdcById()
+
+	return &schema.Resource{
+		CreateContext: resourceAffinityGroupCreate,
+		ReadContext:   resourceAffinityGroupRead,
+		UpdateContext: resourceAffinityGroupUpdate,
+		DeleteContext: resourceAffinityGroupDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceAffinityGroupImport,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: args,
+	}
+}
+
+func resourceAffinityGroupCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	manager := meta.(*CombinedConfig).Manager()
+
+	targetVdc, err := GetVdcById(d, manager)
+	if err != nil {
+		return diag.Errorf("[ERROR-042]: crash via getting VDC: %s", err)
+	}
+
+	config := struct {
+		VdcId       string
+		Name        string
+		Description string
+		Policy      string
+		Reboot      bool
+		vms         []*bcc.MetaData
+	}{
+		VdcId:       d.Get("vdc_id").(string),
+		Name:        d.Get("name").(string),
+		Description: d.Get("description").(string),
+		Policy:      d.Get("policy").(string),
+		Reboot:      d.Get("reboot").(bool),
+		vms:         d.Get("vms").([]*bcc.MetaData),
+	}
+
+	newAffGp := bcc.NewAffinityGroup(config.Name, config.Description, config.Policy, config.vms)
+	newAffGp.Reboot = config.Reboot
+
+	if err := targetVdc.CreateAffinityGroup(&newAffGp); err != nil {
+		return diag.Errorf("[ERROR-042]: crash via creating AffinityGroup: %s", err)
+	}
+
+	if err = newAffGp.WaitLock(); err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(newAffGp.ID)
+
+	return resourceAffinityGroupRead(ctx, d, meta)
+}
+
+func resourceAffinityGroupRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	manager := meta.(*CombinedConfig).Manager()
+
+	affGroup, err := manager.GetAffinityGroup(d.Id())
+	if err != nil {
+		if err.(*bcc.ApiError).Code() == 404 {
+			d.SetId("")
+			return nil
+		} else {
+			return diag.Errorf("[ERROR-042]: crash via getting AffinityGroup: %s", err)
+		}
+	}
+
+	fields := map[string]interface{}{
+		"vdc_id":      affGroup.Vdc.ID,
+		"name":        affGroup.Name,
+		"description": affGroup.Description,
+		"policy":      affGroup.Policy,
+		"reboot":      affGroup.Reboot,
+		"vms":         affGroup.Vms,
+	}
+
+	if err := setResourceDataFromMap(d, fields); err != nil {
+		return diag.Errorf("[ERROR-042]: crash via setting resource data: %s", err)
+	}
+
+	return nil
+}
+
+func resourceAffinityGroupUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	manager := meta.(*CombinedConfig).Manager()
+	needUpdate := false
+
+	affGroup, err := manager.GetAffinityGroup(d.Id())
+	if err != nil {
+		return diag.Errorf("[ERROR-042]: crash via getting AffinityGroup: %s", err)
+	}
+
+	if d.HasChange("name") {
+		needUpdate = true
+		affGroup.Name = d.Get("name").(string)
+	}
+
+	if d.HasChange("description") {
+		needUpdate = true
+		affGroup.Description = d.Get("description").(string)
+	}
+
+	if d.HasChange("reboot") {
+		needUpdate = true
+		affGroup.Reboot = d.Get("reboot").(bool)
+	}
+
+	if d.HasChange("vms") {
+		needUpdate = true
+		affGroup.Vms = d.Get("vms").([]*bcc.MetaData)
+	}
+
+	if needUpdate {
+		if err := repeatOnError(affGroup.Update, affGroup); err != nil {
+			return diag.Errorf("[ERROR-042]: crash via updating AffinityGroup: %s", err)
+		}
+	}
+
+	return resourceAffinityGroupRead(ctx, d, meta)
+}
+
+func resourceAffinityGroupDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	manager := meta.(*CombinedConfig).Manager()
+
+	affGroup, err := manager.GetAffinityGroup(d.Id())
+	if err != nil {
+		return diag.Errorf("[ERROR-042]: crash via getting AffinityGroup: %s", err)
+	}
+
+	if err = affGroup.Delete(); err != nil {
+		return diag.Errorf("[ERROR-042]: crash via deleting vm: %s", err)
+	}
+	affGroup.WaitLock()
+
+	return nil
+}
+
+func resourceAffinityGroupImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	manager := meta.(*CombinedConfig).Manager()
+
+	affGroup, err := manager.GetAffinityGroup(d.Id())
+	if err != nil {
+		return nil, fmt.Errorf("[ERROR-042]: crash via getting AffinityGroup: %s", err)
+	}
+
+	d.SetId(affGroup.ID)
+
+	return []*schema.ResourceData{d}, nil
+
+}
